d9e431c1aab8eb946c2a447ecacf9266155455ee
Iker Blanco <37939870+ikerblac@users.noreply.github.com>
2022-11-03 21:26:25 +0100

    test

---
+++ edit.php
@@ -0,0 +1,74 @@
+<?php 
+session_start();
+include "src/php/db_config.php";
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+ 
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Coches</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="src/css/home.css" />
+    <meta name="author" content="ikerblac">
+    <link rel="shortcut icon" href="src/img/logo_portaventura.png" />
+    <?php 
+        $query_info_hotel="SELECT * FROM hotels ";
+        $result_info_hotel=mysqli_query($connection_db,$query_info_hotel);
+    ?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/home.php">Inicio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="src/php/login_system/logout.php">Cerrar sesion</a>
+                    </li>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid p-5 bg-default text-white text-center ">
+        <img src="src/img/logo_portaventura.png" width="25%" />
+        <h3>
+            Hola,
+            <?php echo $_SESSION['user_name']; ?>
+        </h3>
+    </div>
+
+    <div class="container mt-5">
+        <!-- <a href="add_form.php" class="btn btn-primary">Crear nou curs</a> -->
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-12">
+                
+        </div>
+    </div>
+</body>
+
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
---
+++ src/php/edit_system/delete.php
@@ -0,0 +1,20 @@
+<?php
+	include ('connexioBD.php');
+	//revem el id per el formulari anterior per get
+	$id=$_GET["ID_habitacio"];
+	
+	//consulta		
+	$sql="select * from hotels where ID_habitacio = '$id'";
+	//consulta per eliminar
+	$delete="DELETE FROM hotels where ID_habitacio ='$id'";
+	//eliminem la taula en questio per el id 
+	$resultatdelete = mysqli_query($conn,$delete);
+	//control per saber si s'ha eliminat i mostrem un enllaç per tornar al fitxer anterior
+	if($resultatdelete){
+		header("Location: ../../../home.php?solution=Eliminado correctamente");
+	    exit();
+	}else{
+		echo "No s'ha pogut eliminar!!";
+	}
+
+?>
