8abe823c946c755a699454d3c2aaeb8be34cd17b
Iker Blanco <37939870+ikerblac@users.noreply.github.com>
2022-11-04 19:15:54 +0100

    test

--- src/php/edit_system/delete.php
+++ src/php/edit_system/delete.php
@@ -3,18 +3,16 @@
 	//revem el id per el formulari anterior per get
 	$id=$_GET["id"];
 	echo $id;
-	/*
-	//consulta		
-	$sql="select * from hotels where ID_habitacio = '$id'";
+	
 	//consulta per eliminar
 	$delete="DELETE FROM hotels where ID_habitacio ='$id'";
 	//eliminem la taula en questio per el id 
-	$resultatdelete = mysqli_query($conn,$delete);
+	$resultatdelete = mysqli_query($connection_db,$delete);
 	//control per saber si s'ha eliminat i mostrem un enllaç per tornar al fitxer anterior
 	if($resultatdelete){
 		echo "Listo";
 	}else{
 		echo "No s'ha pogut eliminar!!";
 	}
-	*/
+	
 ?>
