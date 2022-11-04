d9d01310da4af867809d6e35a210662354ddb840
Iker Blanco <37939870+ikerblac@users.noreply.github.com>
2022-11-04 19:22:29 +0100

    test

--- src/php/edit_system/delete.php
+++ src/php/edit_system/delete.php
@@ -7,10 +7,10 @@
 	$result=mysqli_query($connection_db,$query_delete);
 	
 	if($result){
-		header("Location: ../../../index.php?info=Soilicitad porcesada correctamente");
+		header("Location: ../../../home.php?info=Soilicitad porcesada correctamente");
 	    exit();
 	}else{
-		header("Location: ../../../index.php?info=No s'ha pogut eliminar!!");
+		header("Location: ../../../home.php?info=No s'ha pogut eliminar!!");
 	    exit();
 	}
 	
